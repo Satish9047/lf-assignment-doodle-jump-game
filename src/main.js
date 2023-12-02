11c8dfdc64bc1612d4588cc240ce1b0f9d090bef
Satish9047 <satishprajapati930@gmail.com>
2023-12-02 19:58:02 +0545

    added background and the doodle

--- src/main.js
+++ src/main.js
@@ -1,122 +1,51 @@
-// Creating the canvas and context
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-canvas.width = 320;
-canvas.height = 512;
+// Canvas properties
+let canvasHeight = 512;
+let canvasWidth = 320;
+let ctx;
 
-// Defining constants
-const gravity = 0.4;
+// Doodle properties
+let doodleWidth = 50;
+let doodleHeight = 50;
+let doodleX = canvasWidth / 2 - doodleWidth / 2;
+let doodleY = canvasHeight * 7 / 8 - doodleHeight;
 
-//defining the platform
-const platformArray = [];
-let platformHeight = 18;
-let platformWidth = 60;
-let platformImg;
-platformImg = new Image();
-platformImg.src = "./image/doodle-char-1-L-x1.png";
-
-
-// Creating the background image
-const backgroundImage = new Image();
-backgroundImage.src = '../src/image/bck.png';
-
-backgroundImage.onload = function () {
-  animate();
+// Doodle object
+let doodle = {
+    x: doodleX,
+    y: doodleY,
+    img: null,
+    width: doodleWidth,
+    height: doodleHeight
 };
 
-// Creating the Player class
-class Player {
-  constructor(position) {
-    this.position = position;
-    this.velocity = {
-      x: 0,
-      y: 1,
+window.onload = function () {
+    let canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    // Draw a green rectangle
+    ctx.fillStyle = "green";
+    ctx.fillRect(doodle.x, doodle.y, doodle.width, doodle.height);
+
+    // Load image
+    let doodleRightImg = new Image();
+    doodleRightImg.src = "../src/image/doodlestein-right@2x.png";
+
+    // Wait for the image to load
+        doodleRightImg.onload = function () {
+        doodle.img = doodleRightImg;
+
+        // Clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Draw the image
+        ctx.drawImage(doodle.img, doodle.x, doodle.y, doodle.width, doodle.height);
     };
-    this.height = 50;
-  }
-
-  draw() {
-    ctx.fillStyle = "red"; // Set the fill style
-    ctx.fillRect(this.position.x, this.position.y, 50, this.height);
-  }
-
-  update() {
-    this.draw();
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-
-    // Check if the player has reached the left or right boundary
-    if (this.position.x < 0) {
-        this.position.x = canvas.width; // Move to the right side
-      } else if (this.position.x > canvas.width) {
-        this.position.x = 0; // Move to the left side
-      }
-
-
-    //check if the player reach at the bottom of the canvas
-    if (this.position.y + this.height + this.velocity.y < canvas.height) {
-      this.velocity.y += gravity;
-    } else {
-      this.velocity.y = 0;
-    }
-  }
-}
-
-const player = new Player({
-  x: 200,
-  y: 200,
-});
-
-// Key object to track key events
-const key = {
-  d: { pressed: false },
-  a: { pressed: false },
 };
 
-// Animation function
-function animate() {
-  window.requestAnimationFrame(animate);
 
-  // Draw the background image only once
-  ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
 
-  // Update and draw other elements
-  player.update();
-  player.velocity.x = 0;
 
-  if (key.d.pressed) {
-    player.velocity.x = 5;
-  } else if (key.a.pressed) {
-    player.velocity.x = -5;
-  }
-}
 
-// Event listeners for keydown and keyup
-window.addEventListener("keydown", (event) => {
-  switch (event.key) {
-    case "d":
-      key.d.pressed = true;
-      break;
-    case "a":
-      key.a.pressed = true;
-      break;
-    case "w":
-      player.velocity.y = -12;
-      break;
-    default:
-      break;
-  }
-});
-
-window.addEventListener("keyup", (event) => {
-  switch (event.key) {
-    case "d":
-      key.d.pressed = false;
-      break;
-    case "a":
-      key.a.pressed = false;
-      break;
-    default:
-      break;
-  }
-});
